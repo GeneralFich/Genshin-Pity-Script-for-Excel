c2f7ef3ec6e9183d62e667045e56a1557e31398a
GeneralFich <charliefeng1993@gmail.com>
2021-01-24 18:22:21 -0500

    gui implementation 20% done

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+from tkinter import *
 from pity_calculator import PityCalculator
+
+# TODO: Make GUI look good
+# TODO: Link GUI elements to variables, functions, and objects
+# TODO: Store default values for file name, etc. in .txt file
+
+window = Tk()
+window.title("Genshin Pity Script for Excel")
+window.minsize(width=400, height=600)
+
+title_label = Label(text="Genshin Pity Script Runner",
+                    font=("Arial", 12, "bold"),
+                    anchor="Left")
+title_label.pack()
 
 # USER MANUAL:
 # 1) Store the Excel workbook in the same folder as this python file.
 # 2) Change the file_name constant below to the name of the Excel workbook.
 # 3) Change the sheet_name constant below to the name of the worksheet
 # that contains the log of summon data.
+
+filename_label = Label(text="Enter Excel file name with extension:",
+                       font=("Arial", 12))
+filename_label.pack()
+filename_field = Entry()
+filename_field.pack()
+
 file_name = "genshin.xlsx"
 sheet_name = "Summon_Log"
 
@@ -20,3 +41,5 @@ starlvl_col_header = "Star_Level"
 calculator = PityCalculator(
     file_name, sheet_name, banner_col_header, starlvl_col_header
 )
+
+window.mainloop()
